c8458294dcc762d04c0ed4767c7d1a458c063502
Luke Beer <eat.lemons@gmail.com>
2012-10-30 15:49:02 +0000

    Addition of schema XSDs + XSD2PHP

--- example.php
+++ example.php
@@ -3,11 +3,8 @@ require_once 'common.php';
 
 $enterpriseId = 'Enterprise-1';
 $groupId      = 'Group-1';
+$msg          = OCISchemaUser::UserGetListInGroupRequest($enterpriseId, $groupId);
+$xml          = $OCIClient->ociBuilder->build($msg);
+$response     = ($OCIClient->send($xml)) ? $OCIClient->getXMLResponseBody() : $errorControl->getErrors();
 
-$UserGetListInGroupRequest     = OCISchemaUser::UserGetListInGroupRequest($enterpriseId, $groupId);
-$UserGetListInGroupRequestXML  = $OCIClient->ociBuilder->build($UserGetListInGroupRequest);
-$UserGetListInGroupResponse    = ($OCIClient->send($UserGetListInGroupRequestXML))
-                                    ? $OCIClient->getXMLResponseBody()
-                                    : $errorControl->getErrors();
-
-print_r($UserGetListInGroupResponse);
+print_r($response);
