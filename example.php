b1c4d88bb4bbced9e67b4a92e48bae364c49dca4
LukeBeer <eat.lemons@gmail.com>
2012-11-18 19:45:03 +0000

    Updated example to remove OCIBuilder

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
+// Example to print the user list in an enterprises group.
 require_once 'common.php';
-$msg          = OCISchemaUser::UserGetListInGroupRequest('Enterprise-1', 'Group-1');
-$xml          = $OCIClient->ociBuilder->build($msg);
-$response     = ($OCIClient->send($xml)) ? $OCIClient->getXMLResponseBody() : $errorControl->getErrors();
+$msg      = OCISchemaUser::UserGetListInGroupRequest('Enterprise-1', 'Group-1');
+$response = ($OCIClient->send($msg)) ? $OCIClient->getResponse() : $errorControl->getErrors();
 print_r($response);
