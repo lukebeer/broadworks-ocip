152e415c7e4c6a827e45bfc40f08fbf01dd35111
lukebeer <eat.lemons@gmail.com>
2012-11-20 23:18:46 +0000

    Updated common file

--- common.php
+++ common.php
@@ -3,11 +3,7 @@ ini_set('display_errors', 0);
 libxml_use_internal_errors(true);
 error_reporting(E_ALL);
 define('OCI_PATH', realpath(dirname(__FILE__)));
-
 require_once(OCI_PATH . "/corefactory.php");
 require_once(OCI_PATH . "/factory.php");
-
-Factory::getOCISchemaLogin();
-Factory::getOCISchemaUser();
 $errorControl = &CoreFactory::getErrorControl();
 $OCIClient    = CoreFactory::getOCIClient('http://example.com/webservice/services/ProvisioningService', 'username', 'password');
