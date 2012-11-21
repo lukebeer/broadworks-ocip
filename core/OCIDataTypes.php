6efca92b611beb9ff5590b59bb099470fac30915
LukeBeer <eat.lemons@gmail.com>
2012-11-21 22:37:50 +0000

    Update core/OCIDataTypes.php
    Added OCISearchModes

--- core/OCIDataTypes.php
+++ core/OCIDataTypes.php
@@ -3,3 +3,9 @@ class OCIDataTypes {
     const OCI_NAME   = 'Name';
     const OCI_PARAMS = 'Params';
 }
+
+class OCISearchModes {
+    const STARTS_WITH = 'Starts With';
+    const CONTAINS    = 'Contains';
+    const EQUAL       = 'Equal To';
+}
