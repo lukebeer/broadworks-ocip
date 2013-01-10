b47648767f8d87963cf5c3a5ab80fe5e1a8b006a
Luke Beer <eat.lemons@gmail.com>
2013-01-10 17:32:35 +0000

    Added number validation

--- examples/customCLIValidator.php
+++ examples/customCLIValidator.php
@@ -1,9 +1,10 @@
 <?php
-require_once '../common.php';
+require_once '../Broadworks_OCI-P/common.php';
+ini_set("max_execution_time", 0);
 
 class ValidateCustomCLI {
 
-    public $numbers = false;
+    public $numbers = [];
     public $structure = false;
 
     public function __construct($user=null, $pass=null, $host='http://example.com/webservice/services/ProvisioningService') {
@@ -44,8 +45,31 @@ class ValidateCustomCLI {
         return false;
     }
 
+    // Returns array of user list in Group.
+    public function getUsersInGroup($serviceProvider, $group) {
+        $msg = OCISchemaUser::UserGetListInGroupRequest($serviceProvider, $group);
+        $this->client->send($msg);
+        $users = null;
+        if ($response = $this->client->getResponse()) {
+            foreach ($response->userTable['row'] as $row) {
+                $users[] = isset($row['col']) ? $row['col'][0] : $row[0];
+            }
+        }
+        return $users;
+    }
+
+    // Return DN configured on user profile or false
+    public function checkUserCallingLineID($user) {
+        $msg = OCISchemaUser::UserGetRequest17sp4($user);
+        $this->client->send($msg);
+        if ($response = $this->client->getResponse()) {
+            return (isset($response->callingLineIdPhoneNumber)) ? $response->callingLineIdPhoneNumber : false;
+        }
+        return false;
+    }
+
     // Get scope of Group Configurable CLID and check setting, return Boolean.
-    public function getCLIDConfig($serviceProvider, $group) {
+    public function checkCLIDConfig($serviceProvider, $group) {
         $msg = OCISchemaGroup::GroupCallProcessingGetPolicyRequest17sp4($serviceProvider, $group);
         $this->client->send($msg);
         if ($response = $this->client->getResponse()) {
@@ -58,6 +82,22 @@ class ValidateCustomCLI {
         return false;
     }
 
+    // Validates if the number exists in the Service Provider
+    public function validateNumber($clid, $serviceProvider) {
+        $numbers = $this->getNumbersInServiceProvider($serviceProvider);
+        if ((substr($clid, 0, 1) == 0) && (substr($clid, 0, 2) != 00)) {
+            $clidNoPrefix = substr($clid, 1);
+            $len = strlen($clidNoPrefix);
+            $found = false;
+            foreach ($numbers as $number) {
+                if ((substr($number, -$len) == $clidNoPrefix)) {
+                    $found = true;
+                }
+            }
+            return $found;
+        }
+    }
+
     // Return array of numbers in Service Provider, expanded, NOT ranges.
     public function getNumbersInServiceProvider($serviceProvider) {
         $numbers = null;
@@ -65,6 +105,7 @@ class ValidateCustomCLI {
         $this->client->send($msg);
         if ($response = $this->client->getResponse()) {
             foreach($response->dnSummaryTable['row'] as $row) {
+                if (!isset($row['col'][0])) { var_dump($row); die();}
                 if (preg_match('/ - /', $row['col'][0])) {
                     $item = str_replace(['-', '+'], null, $row['col'][0]);
                     preg_match('/(\d+)\s+(\d+)/', $item, $range);
@@ -74,9 +115,8 @@ class ValidateCustomCLI {
                     }
                 }
             }
-            return $this->numbers;
         }
-        return false;
+        return $this->numbers;
     }
 
     public function run() {
@@ -84,9 +124,18 @@ class ValidateCustomCLI {
             $this->structure[$serviceProvider] = $this->getGroupsInServiceProvider($serviceProvider);
         }
         foreach ($this->structure as $sp => $grps) {
-            foreach ($grps as $grp) {
-                if ($this->getCLIDConfig($sp, $grp)) {
-                    echo "$sp - $grp uses configurable CLID\n";
+            if (isset($grps)) {
+                foreach ($grps as $grp) {
+                    if ($this->checkCLIDConfig($sp, $grp)) {
+                        echo "$sp - $grp uses configurable CLID, checking users.....\n";
+                        foreach ($this->getUsersInGroup($sp, $grp) as $user) {
+                            if ($clid = $this->checkUserCallingLineID($user)) {
+                                if (!$this->validateNumber($clid, $sp)) {
+                                    echo "\tWarning: $user - $clid Does not match any assigned numbers\n";
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
