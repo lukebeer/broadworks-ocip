3bdf30b2a74642960d93a55d5ddd25d9d97156ea
NaN0 <pillock@rapid.uniqueicthosting.com>
2013-08-05 23:59:27 +0100

    Missing OCIBuilder for OCIClient

--- utils/OCIJobControl.php
+++ utils/OCIJobControl.php
@@ -15,6 +15,7 @@ class OCIPJobControl extends OCIClient {
         parent::__construct($host, false);
         if ($session) {
             $this->setSession($session);
+            $this->ociBuilder = CoreFactory::getOCIBuilder($this->getSessionId());
             $this->send(OCISchemaLogin::VerifySessionIsValidRequest());
             if (!$this->getResponse()) {
                 die($this->errorControl->getLastError());
