efc3282ada9224095708c2e65c610b7fedc48005
Luke Beer <eat.lemons@gmail.com>
2013-08-07 22:54:30 +0100

    Removed functions outside of ErrorControl's scope

--- core/ErrorControl.php
+++ core/ErrorControl.php
@@ -20,29 +20,7 @@ class ErrorControl {
         $this->lastError= null;
     }
 
-    public function getErrorList($tabWidth) {
-        if ($this->errors == null) return false;
-        $html = "$tabWidth\t<ul>\n" .
-            "$tabWidth\t\t<li>" .
-            implode("</li>\n$tabWidth\t\t<li>", $this->errors) .
-            "</li>\n$tabWidth\t</ul>\n";
-        return $html;
-    }
-
     public function hasErrors() {
         return ($this->errors != null);
-    }
-
-    public function getErrorHtml($tabWidthCount) {
-        $tabWidth = (is_numeric($tabWidthCount)) ? str_repeat("\t", $tabWidthCount) : "";
-        if ($this->errors != null) {
-            $html  = "$tabWidth<div id=\"exceptions\">\n";
-            $html .= "$tabWidth\t<h3>:: EXCEPTIONS ::</h3>\n";
-            $html .= $this->getErrorList($tabWidth);
-            $html .= "$tabWidth</div>\n";
-            return $html;
-        } else {
-            return false;
-        }
     }
 }
