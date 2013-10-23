51a6c853e9f5c9eb02ead63a8ab0771494704f63
Luke Beer <eat.lemons@gmail.com>
2013-10-23 12:03:13 +0100

    Added match option as filter

--- utils/OCIParser.php
+++ utils/OCIParser.php
@@ -1,15 +1,13 @@
 <?php
-# This file is a complete hack
-function xsd2php($file) {
+function xsd2php($file, $match='Request') {
     $xml = file_get_contents($file);
     $xml = simplexml_load_string($xml, 'SimpleXMLElement', 0, "xs", true);
     $className = basename($file, ".xsd");
     $code = "<?php\nclass $className {\n";
-
     if (is_object($xml)) {
         foreach ($xml->complexType as $complexType) {
             $name = $complexType->attributes()[0];
-            if (preg_match('/Request/', $name, $matches)) {
+            if (preg_match("/$match/", $name)) {
                 preg_match('/(\w+.*?)\n/', $complexType->annotation->documentation, $docString);
                 $params = null;
                 $paramsArray = null;
@@ -22,13 +20,9 @@ function xsd2php($file) {
                             }
                         }
                     }
-                } else {
-                    // echo "No elements.\n";
                 }
-
                 $params = (substr($params, -1) == ',') ? substr($params, 0, -1) : $params;
                 $paramsArray = (substr($paramsArray, -2, 1) == ',') ? substr($paramsArray, 0, -2) : $paramsArray;
-
                 $code .= "        # $docString[1]\n";
                 $code .= "        static public function $name($params) {\n";
                 $code .= "            return [\n";
@@ -66,14 +60,16 @@ function getXSDs($dir) {
     return $files;
 }
 
-if ($files = getXSDs("/home/l_berezynskyj/Broadworks_OCI-P/api/xsd/Rel_17.sp4_1.197_OCISchemaAS/")) {
-    foreach ($files as $file) {
-        $file = str_replace("./", "", $file);
-        $newFile = substr($file, 0, -3) . "php";
-        $newFile = str_replace("xsd/Rel_17.sp4_1.197_OCISchemaAS/", "php/Rel_17.sp4_1.197_OCISchemaAS/", $newFile);
-        $code = xsd2php($file);
-        $fh = fopen($newFile, 'w');
-        fwrite($fh, $code);
-        fclose($fh);
+function run($path='/path/to/xsd/Rel_17.sp4_1.197_OCISchemaAS/') {
+    if ($files = getXSDs($path)) {
+        foreach ($files as $file) {
+            $file = str_replace("./", "", $file);
+            $newFile = substr($file, 0, -3) . "php";
+            $newFile = str_replace("xsd/Rel_17.sp4_1.197_OCISchemaAS/", "php/Rel_17.sp4_1.197_OCISchemaAS/", $newFile);
+            $code = xsd2php($file);
+            $fh = fopen($newFile, 'w');
+            fwrite($fh, $code);
+            fclose($fh);
+        }
     }
 }
