cb3606d869bbc7030f58dc86af2aa2efbca422f5
LukeBeer <eat.lemons@gmail.com>
2015-03-06 21:14:21 +0000

    Corrected restriction to validate input length

--- core/Builder/Restrictions/Length.php
+++ core/Builder/Restrictions/Length.php
@@ -12,6 +12,11 @@ class Length extends Restriction implements RestrictionInterface
 {
     public function validate($input)
     {
-        return in_array($input, $this->value);
+        $length = strlen($input);
+        $valid = (strlen($input) == (int)$this->value);
+        ($valid)
+            ? $this->detail("Valid: String length of $length is valid, required length is {$this->value}")
+            : $this->detail("Failed: String length of $length is invalid, required length is {$this->value}");
+        return $valid;
     }
 }
