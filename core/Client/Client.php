65d98440ccc69f5ea8a80006dad844a603a560f0
LukeBeer <eat.lemons@gmail.com>
2015-03-12 21:23:41 +0000

    Added documentation

--- core/Client/Client.php
+++ core/Client/Client.php
@@ -11,19 +11,23 @@ namespace Broadworks_OCIP\core\Client;
 use Broadworks_OCIP\api\Rel_17_sp4_1_197_OCISchemaAS\OCISchemaLogin\AuthenticationRequest;
 use Broadworks_OCIP\api\Rel_17_sp4_1_197_OCISchemaAS\OCISchemaLogin\LoginRequest14sp4;
 use Broadworks_OCIP\api\Rel_17_sp4_1_197_OCISchemaAS\OCISchemaLogin\LogoutRequest;
-use Broadworks_OCIP\core\Builder\Types\ComplexType;
 use Broadworks_OCIP\core\Client\Transport\TransportInterface;
+use Broadworks_OCIP\core\Builder\Types\ComplexType;
 use Broadworks_OCIP\core\Console\CommandGenerator;
-use Broadworks_OCIP\core\Console\Console;
 use Broadworks_OCIP\core\Response\ResponseOutput;
+use Broadworks_OCIP\core\Output\OutputInterface;
+use Broadworks_OCIP\core\Console\Console;
 use Broadworks_OCIP\CoreFactory;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\SimpleXMLElement;
 
 
 require_once 'HTTP/Request2.php';
 
-
+/**
+ * Class Client is the main controller for the whole framework and interface to the Broadworks API
+ *
+ *
+ * @package Broadworks_OCIP\core\Client
+ */
 class Client
 {
     public $ociBuilder = null;
@@ -34,7 +38,13 @@ class Client
     private $responseType = null;
     private $appendToResponse = [];
 
-
+    /**
+     * Constructor, gateway to another dimension.
+     * Creates and sets a new session and command builder instance
+     * Sets the logging interface
+     *
+     * @param TransportInterface $transport
+     */
     public function __construct(TransportInterface $transport)
     {
         $this->transport = $transport;
@@ -44,6 +54,9 @@ class Client
         $this->transport->updateSession($this->session);
     }
 
+    /**
+     * Destructor will send a LogoutRequest if autoLogout is true.
+     */
     public function __destruct()
     {
         if ($this->autoLogout) {
@@ -52,6 +65,11 @@ class Client
         }
     }
 
+    /**
+     * Sends a LogoutRequest to Broadworks and destroys session.
+     *
+     * @return void
+     */
     public function logout()
     {
         if ($this->session->getLoggedIn()) {
@@ -61,6 +79,13 @@ class Client
         }
     }
 
+    /**
+     * If required, generates the XML message from a ComplexType object.
+     * Method send() is called on the Transport object to send data to Broadworks.
+     *
+     * @param ComplexType|string $msg
+     * @return mixed
+     */
     public function send($msg)
     {
         $this->requestMsg = $msg;
@@ -70,6 +95,14 @@ class Client
         return $this->transport->send($msg);
     }
 
+    /**
+     * Second and final stage of login process.
+     * Sends a LoginRequest to attempt authentication with Broadworks.
+     *
+     * @param null $userId
+     * @param null $pass
+     * @return bool
+     */
     public function login($userId = null, $pass = null)
     {
         if (isset($userId) && (isset($pass))) {
@@ -85,6 +118,13 @@ class Client
         return false;
     }
 
+    /**
+     * First stage of login process.
+     * Sends initial AuthenticationRequest to obtain nonce used for LoginRequest.
+     *
+     * @param null $userId
+     * @return bool
+     */
     public function authenticate($userId = null)
     {
         if ($userId) $this->session->setUserId($userId);
@@ -96,17 +136,35 @@ class Client
         return false;
     }
 
+    /**
+     * Provides method to obtain the last response from Broadworks and return requested response type.
+     *
+     * @param int $outputType
+     * @param bool $responseType
+     * @return mixed
+     */
     public function getResponse($outputType = ResponseOutput::STD, $responseType=false)
     {
         $responseType = $responseType ?: $this->responseType;
         return $this->transport->getResponse($responseType, $outputType, $this->appendToResponse);
     }
 
+    /**
+     * Returns session object.
+     *
+     * @return \Broadworks_OCIP\core\Session\Session|null
+     */
     public function getSession()
     {
         return $this->session;
     }
 
+    /**
+     * Sets session object.
+     *
+     * @param $session
+     * @return mixed
+     */
     public function setSession($session)
     {
         $this->session = $session;
@@ -114,21 +172,41 @@ class Client
         return $session;
     }
 
+    /**
+     * Returns last request message.
+     *
+     * @return null
+     */
     public function getLastRequestMsg()
     {
         return $this->requestMsg;
     }
 
+    /**
+     * Returns last response message.
+     *
+     * @return mixed
+     */
     public function getLastResponseMsg()
     {
         return $this->transport->getLastResponseBody();
     }
 
+    /**
+     * Returns session id.
+     *
+     * @return null|number
+     */
     public function getSessionId()
     {
         return $this->session->getSessionId();
     }
 
+    /**
+     * Initializes interactive OCIP console as CLI.
+     *
+     * @param OutputInterface $outputInterface
+     */
     public function console(OutputInterface $outputInterface)
     {
         $commandGenerator = new CommandGenerator(OCI_PATH . '/api/php/Rel_17.sp4_1.197_OCISchemaAS');
@@ -136,16 +214,32 @@ class Client
         $console->init();
     }
 
+    /**
+     * Sets timeout on transport for pending requests.
+     *
+     * @param int $timeout
+     */
     public function setTimeout($timeout = 4)
     {
         $this->timeout = $timeout;
     }
 
+    /**
+     * Appends additional elements into response before serializing into requested response type.
+     * Used so responses such as userGetResponse17sp4 can be automatically mapped into a userAddRequest17sp4 for example.
+     * In this case, a userAddRequest17sp4 requires element userId however userGetResponse17sp4 doesn't include it.
+     *
+     * @param \Broadworks_OCIP\core\Builder\Types\SimpleType|ComplexType $element
+     * @return $this
+     */
     public function addElementToResponse($element) {
         $this->appendToResponse[] = $element;
         return $this;
     }
 
+    /**
+     * Empties appendToResponse array.
+     */
     public function clearAdditionalResponseElements() {
         $this->appendToResponse = [];
     }
