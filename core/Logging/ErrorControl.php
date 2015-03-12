9a576eee22ba9c9bc7a58ca6070a5bc02265933c
LukeBeer <eat.lemons@gmail.com>
2015-03-12 21:52:56 +0000

    Added documentation for ErrorControl

--- core/Logging/ErrorControl.php
+++ core/Logging/ErrorControl.php
@@ -7,32 +7,61 @@
 
 namespace Broadworks_OCIP\core\Logging;
 
+/**
+ * Crappy logging object, needs nuking and starting again.
+ *
+ * Class ErrorControl
+ * @package Broadworks_OCIP\core\Logging
+ */
 class ErrorControl
 {
     var $errors = null;
     var $lastError = null;
 
+    /**
+     * Add error into errors array.
+     *
+     * @param $error
+     */
     public function addError($error)
     {
         $this->errors[] = $error;
         $this->lastError = $error;
     }
 
+    /**
+     * Return errors.
+     *
+     * @return array
+     */
     public function getErrors()
     {
         return $this->errors;
     }
 
+    /**
+     * Return last error.
+     *
+     * @return null
+     */
     public function getLastError()
     {
         return $this->lastError;
     }
 
+    /**
+     * Clear last error.
+     */
     public function clearLastError()
     {
         $this->lastError = null;
     }
 
+    /**
+     * Check if object has errors.
+     *
+     * @return bool
+     */
     public function hasErrors()
     {
         return ($this->errors != null);
